c23161072d7ad162b03575ed36bb4404d72a074c
Angelo261295 <angeloburgio@hotmail.it>
2022-04-11 23:14:48 +0200

    piccola modifica a postcard

---
+++ components/postcard.php
@@ -0,0 +1,12 @@
+<?php
+
+?>
+
+
+<div class="<?= $cardsaward['classbootstrap']?>">
+    <div class="<?= $cardsaward['classes'] ?>">
+        <div><img src="<?= $cardsaward['image'] ?>" alt=""></div>
+        <h4 class="<?= $cardsaward['classtitle'] ?>"><?= $cardsaward['title'] ?></h4>
+        <div class="<?= $cardsaward['classsubtitle'] ?>"><?= $cardsaward['subtitle'] ?></div>
+    </div>
+</div>   
