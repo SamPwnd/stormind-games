21ab5686572b6d47bc018494ec6e04360988f7cc
MarianoPd <marianopidone@gmail.com>
2022-04-11 11:55:32 +0200

    completata navbar

--- mariano.php
+++ mariano.php
@@ -7,7 +7,13 @@
         echo $text;
     }
 
-
+    $navbar =[
+        'classes' => 'navbar',
+        'logo' => './assets/logo.svg',
+        'navwrapper' => 'navbar__wrapper',
+        'navlogo' => 'navbar__logo',
+        
+    ];
 
 ?>
 
@@ -23,9 +29,27 @@
 <body>
     <div class="mp-body">
         <h1>ciao</h1>
+        <img src="./assets/logo.svg" alt="">
     </div>
 
-    <?php echo get_template_part('nabar', ['ciao' => 'ciao']) ?>
+    <?php ob_start(); ?>
+        <ul class="navbar__links">
+            <li class="navbar__link"><a href="#">company</a></li>
+            <li class="navbar__link"><a href="#">games</a></li>
+            <li class="navbar__link"><a href="#">news</a></li>
+            <li class="navbar__link"><a href="#">press kit</a></li>
+            <li class="navbar__link"><a href="#">careers</a></li>
+            <li class="navbar__link">
+                <a href="#">
+                    <img src="./assets/icon-Coop.svg" alt="">
+                    work with us
+                </a>
+            </li>
+        </ul>
+    <?php $navbar_content = ob_get_clean(); ?>
+
+
+    <?php  get_template_part('./navbar.php', ['navbar' => $navbar , 'content' => $navbar_content]); ?>
     
 
     
--- navbar.php
+++ navbar.php
@@ -1,10 +1,25 @@
-//  per  ora è messa qui per non cancellare l'esempio navbar
+
 
 <?php
+    /*  per  ora è messa qui per non cancellare l'esempio navbar */
 
+    /*
+    $navbar =[
+        'classes' => 'navbar',
+        'logo' => './assets/logo.svg',  il link andrà cambiato quando si cambierà la posizione del componente
+        'navwrapper' => 'navbar__wrapper',
+        'navlogo' => 'navbar__logo',
+        
+    ];
+
+    */
 
 ?>
 
-<div>
-    <h1>navbar</h1>
+<div class="<?= $navbar['classes'] ?>">
+    <div class="<?= $navbar['navwrapper'] ?>">
+        <img class="<?= $navbar['navlogo'] ?>" src="<?= $navbar['logo'] ?>" alt="<?= $navbar['logo'] ?>" >
+
+        <?= $content ?>
+    </div>
 </div>
