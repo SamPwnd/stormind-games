644a6710e41ed513e851fa2b747297b392918a0a
Samuele Minissale <minissalesamuele@gmail.com>
2022-04-19 13:57:17 +0200

    complete contact form

--- samuele.php
+++ samuele.php
@@ -60,8 +60,12 @@
             'classes' => 'text-panel text-panel--small-title text-panel--black',
             'title' => 'FUNCTIONAL TESTER',
         ],
+    ];
 
-    ]
+    $contact = [
+        'classes' => 'contact',
+
+    ];
     
 
 ?>
@@ -87,6 +91,9 @@
     <?php get_template_part('./components/news.php', ['content' => $news]); ?>
 
     <?php get_template_part('./components/application.php', ['content' => $application]); ?>
+
+    <?php get_template_part('./components/contact.php', ['content' => $contact]); ?>
+
 
     <script src="./script/navbar.js"></script>
     <script src="./script/bootstrap.min.js"></script>
