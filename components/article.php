3d0984d4bda5052b6ab5848c7150ebfdda8c274e
TonyBrb <anthonybarbagallo95@gmail.com>
2022-04-19 17:07:44 +0200

    article e accordion javascript

--- components/article.php
+++ components/article.php
@@ -7,8 +7,11 @@
     <h3><?= $article['title'] ?></h3>
     <p><?= $article['subtitle'] ?></p>
   </div>
-  <div class="line"></div>
-  <div class="container-fluid">
+  <div class="line" id="article_less"></div>
+  <div class="plus d-none" id="article_more">
+    <img src="assets/plus-article.svg" alt="">
+  </div>
+  <div id="description" class="container-fluid description-container">
     <div class="row">
       <div class="col-5">
         <img src="<?= $article['image'] ?>" alt="">
@@ -17,7 +20,8 @@
       <div class="col-12"><?= $article['content-down'] ?></div>
     </div>
   </div>
-  <div class="linkedin">
+  <div class="linkedin" id="linked">
     <img src="assets/linkedin.svg" alt="">
   </div>
+  <div class="line-down" id="line-down-id"></div>
 </div>
