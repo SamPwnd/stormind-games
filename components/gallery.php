d4c2f80ea4f3867fc9facdcdb0cd3c185aa192df
MarianoPd <marianopidone@gmail.com>
2022-04-20 16:51:47 +0200

    modifiche varie

--- components/gallery.php
+++ components/gallery.php
@@ -2,16 +2,29 @@
 
 ?>
 
-<div class="row">
-  <?php foreach($content['elements'] as $element): ?>
+<div class="gallery row">
+  <div class="col-12 d-flex justify-content-center">
+    <div class="gallery__selectors">
+      <a href="#">all</a>
+      <a href="#">screenshot</a>
+      <a href="#">making of</a>
+    </div>
+  </div>
 
+  <?php foreach($content['elements'] as $element): ?>
     <div class="gallery__card col-12 col-md-4 col-lg-3 px-md-3 py-3">
       <div class="gallery__container ratio ratio-1x1">
       <img src="<?= $element?>" style="max-width:100%;cursor:pointer" onclick="onClick(this)" class="">
       </div>
     </div>
   <?php endforeach; ?> 
+
+  <div class="col-12">
+
+  </div>
 </div>
+  
+
 
 
 
