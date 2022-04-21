cdc14a27dad75a99d2941b647b47ba328585f285
TonyBrb <anthonybarbagallo95@gmail.com>
2022-04-21 15:21:48 +0200

    Company-1 awards

---
+++ components/text-company.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+
+<p class="text-company">
+  Released in 2018, Remothered: Tormented Fathers was our first title ever, and on its first year out it was awarded as the “Best Italian Game” and got recognition by several specialized websites such as Rely on Horror (GOTY 2018), Eurogamer (Best Indie and Best Italian Game), and it was considered “one of the scariest games of all time” by Gamespot, just to name a few.
+
+      2018 was also the year in which Stormind Games won the DStars award as “Best New Studio”, voted by more than 100 game professionals!
+
+      Currently, we are developing the second title of the saga, Remothered: Broken Porcelain, that will be released worldwide on Xbox One, PC, Nintendo Switch, and PlayStation 4 on October 20, 2020 and we’re also working on a completely new game, yet unannounced - you will hear about it soon!
+
+      Our marketing-first approach, the intense stories, and the distinctive visuals of our titles are Stormind Games’ trademarks.
+
+      What does this mean?
+
+      First of all, before we start working on a new video game, we always study the market to create games that can be considered unique compared to competitors. They must be suitable to be positioned as distinctive in the players’ minds.
+
+      We aim at raising the bar with each of our titles.
+
+      All of our video games must inspire and captivate players with their intense stories, and they must be immediately recognizable through their distinctive visual style.
+
+      We are based in Catania, Italy and, as of today, our team consists of 33 internal resources and 13 external collaborators among experienced 3D artists, developers, writers, and designers who had the chance to previously prove their skills with CGI movies and video games.  
+</p>
