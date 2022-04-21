748204b4369443c26797573675e82469ba3c4a6e
MarianoPd <marianopidone@gmail.com>
2022-04-21 12:51:35 +0200

    sistemato side-panel

--- components/side-panel.php
+++ components/side-panel.php
@@ -2,14 +2,14 @@
 <div class="side-panel">
    
    <div class="row m-0">
-       <div class="col-10 offset-1 col-md-5" >
+       <div class="<?=$content['content-classes'] ?>" >
            <div class="side-panel__content">
                
                 <?= $text ?>
                 
            </div>
        </div>
-       <div class="col-12 col-md-5 offset-md-1 p-0">
+       <div class=" <?=$content['background-classes'] ?>">
            <div  class="side-panel__background" style="background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('<?= $content['background'] ?>');">
                ciao
                
