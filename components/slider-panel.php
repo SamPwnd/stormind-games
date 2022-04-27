e21ff8ce68f2e81fca30c7734d2f4eb02e6fd4de
MarianoPd <marianopidone@gmail.com>
2022-04-27 09:35:55 +0200

    roba

--- components/slider-panel.php
+++ components/slider-panel.php
@@ -3,44 +3,21 @@
 <div class="container-fluid slider-panel">
     <div class="row">
         <div class="col-12 col-md-6 offset-md-1 ">
-            <p class="slider-panel__title">Interested in a job position at Stormind Games?</p>
-            <p class="slider-panel__description">Here’s what to expect from our recruitment process!<br><br>
- 
-            We like to keep the recruitment experience clear, effective, and straightforward. Here's what you can expect in the different stages of our selection process.</p>
+            <p class="slider-panel__title"><?= $content['title']?></p>
+            <p class="slider-panel__description"><?= $content['description'] ?></p>
         </div>
     </div>
     <div class=" col-12 col-md-10 offset-md-1 slider-panel__wrapper">
         <div class="slider-panel__slider">
-            <div class="slider-panel__element">
-                <p>01.</p>
-                <p>Candidates Pre-selection</p>
-                <span>After the job ad closing date, we start analyzing and selecting the most promising candidates. At this point, we may contact you to talk about the role and schedule an interview.</span>
-            </div>
-            <div class="slider-panel__element">
-                <p>01.</p>
-                <p>Candidates Pre-selection</p>
-                <span>After the job ad closing date, we start analyzing and selecting the most promising candidates. At this point, we may contact you to talk about the role and schedule an interview.</span>
-            </div>
-            <div class="slider-panel__element">
-                <p>01.</p>
-                <p>Candidates Pre-selection</p>
-                <span>After the job ad closing date, we start analyzing and selecting the most promising candidates. At this point, we may contact you to talk about the role and schedule an interview.</span>
-            </div>
-            <div class="slider-panel__element">
-                <p>01.</p>
-                <p>Candidates Pre-selection</p>
-                <span>After the job ad closing date, we start analyzing and selecting the most promising candidates. At this point, we may contact you to talk about the role and schedule an interview.</span>
-            </div>
-            <div class="slider-panel__element">
-                <p>01.</p>
-                <p>Candidates Pre-selection</p>
-                <span>After the job ad closing date, we start analyzing and selecting the most promising candidates. At this point, we may contact you to talk about the role and schedule an interview.</span>
-            </div>
-            <div class="slider-panel__element">
-                <p>01.</p>
-                <p>Candidates Pre-selection</p>
-                <span>After the job ad closing date, we start analyzing and selecting the most promising candidates. At this point, we may contact you to talk about the role and schedule an interview.</span>
-            </div>
+            <?php foreach($content['elements'] as $elements ): ?>
+                <div class="slider-panel__element">
+                    <p><?= $elements['index'] ?></p>
+                    <p><?= $elements['title'] ?></p>
+                    <span><?= $elements['description'] ?></span>
+                </div>
+
+            <?php endforeach;?>
+            
         </div>
     </div>
 </div>
